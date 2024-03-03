df3273e66a177da783f89533fe6fd39bdf154990
Csaba Schnitchen <schcsaba@gmail.com>
2024-03-03 14:47:30 +0100

    update packages

--- tanulmanyi/settings.py
+++ tanulmanyi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=True, cast=bool)
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
